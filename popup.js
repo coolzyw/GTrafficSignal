1bc56757c143c2f95d59611edc4f8f570ee0b3b7
happyzyw <happyzyw@umich.edu>
2018-07-20 12:16:12 -0400

    basically work google drive

--- popup.js
+++ popup.js
@@ -1,29 +1,371 @@
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = "https://apis.google.com/js/api.js";
-head.appendChild(script);
+// var head = document.getElementsByTagName('head')[0];
+// var script = document.createElement('script');
+// script.type = 'text/javascript';
+// script.src = "https://apis.google.com/js/api.js";
+// head.appendChild(script);
 
 var authorizeButton = document.getElementById('authorize_button');
 var signoutButton = document.getElementById('signout_button');
 
-var CLIENT_ID = '630133507196-7ul37fut1n49u3ik6b71a9s18l5t165u.apps.googleusercontent.com';
-var CLIENT_SECRET = '0eRqWj5lk5zESf_QPrTMuHpb';
-var API_KEY = 'AIzaSyB5MWulmdu1sXzj9p6fQ4UGGsucQZ4HwAU';
+/**
+ * GDE Sample: Chrome extension Google APIs.
+ *
+ * This Chrome extension is example code of how to use Chrome's identity API
+ * to get access to Google's web APIs.
+ */
 
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+/**
+ * Create a basic Desktop notification.
+ *
+ * @param {object} options
+ *   @value {string} iconUrl - Image URL to display in notification.
+ *   @value {string} title - Notification header.
+ *   @value {string} message - Notification message.
+ */
+function createBasicNotification(options) {
+  var notificationOptions = {
+      'type': 'basic',
+      'iconUrl': options.iconUrl, // Relative to Chrome dir or remote URL must be whitelisted in manifest.
+      'title': options.title,
+      'message': options.message,
+      'isClickable': true,
+  };
+  chrome.notifications.create(options.id, notificationOptions, function (notificationId) { });
+}
 
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+/**
+* Show a notification that prompts the user to authenticate their Google account.
+*/
+function showAuthNotification() {
+  var options = {
+      'id': 'start-auth',
+      'iconUrl': 'img/developers-logo.png',
+      'title': 'GDE Sample: Chrome extension Google APIs',
+      'message': 'Click here to authorize access to Gmail',
+  };
+  createBasicNotification(options);
+}
+
+/**
+* Show a notification that authentication was completed successfully.
+*
+* @param {object} profile
+*   @value {string} imageUrl - Google+ profile image URL.
+*   @value {string} displayName - Google+ profile full name.
+*/
+function showProfileNotification(profile) {
+  var options = {
+      'id': 'show-profile',
+      'iconUrl': profile.imageUrl,
+      'title': 'Welcome ' + profile.displayName,
+      'message': 'Gmail checker is now active',
+  };
+  createBasicNotification(options);
+}
+
+/**
+* Triggered anytime user clicks on a desktop notification.
+*/
+function notificationClicked(notificationId) {
+  // User clicked on notification to start auth flow.
+  if (notificationId === 'start-auth') {
+      getAuthTokenInteractive();
+  }
+  clearNotification(notificationId);
+}
+
+/**
+* Clear a desktop notification.
+*
+* @param {string} notificationId - Id of notification to clear.
+*/
+function clearNotification(notificationId) {
+  chrome.notifications.clear(notificationId, function (wasCleared) { });
+}
+
+/**
+* Get users access_token.
+*
+* @param {object} options
+*   @value {boolean} interactive - If user is not authorized ext, should auth UI be displayed.
+*   @value {function} callback - Async function to receive getAuthToken result.
+*/
+function getAuthToken(options) {
+  chrome.identity.getAuthToken({ 'interactive': options.interactive }, options.callback);
+}
+
+/**
+* Get users access_token in background with now UI prompts.
+*/
+function getAuthTokenSilent() {
+  getAuthToken({
+      'interactive': false,
+      'callback': getAuthTokenSilentCallback,
+  });
+}
+
+/**
+* Get users access_token or show authorize UI if access has not been granted.
+*/
+function getAuthTokenInteractive() {
+  getAuthToken({
+      'interactive': true,
+      'callback': getAuthTokenInteractiveCallback,
+  });
+}
+
+/**
+* If user is authorized, start getting Gmail count.
+*
+* @param {string} token - Users access_token.
+*/
+function getAuthTokenSilentCallback(token) {
+  // Catch chrome error if user is not authorized.
+  if (chrome.runtime.lastError) {
+      showAuthNotification();
+  } else {
+      updateLabelCount(token);
+  }
+}
+
+/**
+* User finished authorizing, start getting Gmail count.
+*
+* @param {string} token - Current users access_token.
+*/
+function getAuthTokenInteractiveCallback(token) {
+  // Catch chrome error if user is not authorized.
+  if (chrome.runtime.lastError) {
+      showAuthNotification();
+  } else {
+      updateLabelCount(token);
+      //getProfile(token);
+  }
+}
+
+/**
+* Get the current users Google+ profile to welcome them.
+*
+* https://developers.google.com/+/api/latest/people/get
+*
+* @param {string} token - Current users access_token.
+*/
+function getProfile(token) {
+  get({
+      // 'url':'https://www.googleapis.com/',
+      'url': 'https://www.googleapis.com/drive/v3/files?key={YOUR_API_KEY}',
+      'callback': getProfileCallback,
+      'token': token,
+  });
+}
+
+/**
+* Got users Google+ profile, show welcome desktop notification.
+*
+* https://developers.google.com/+/api/latest/people/get
+*
+* @param {object} person - Google+ person resource.
+*/
+function getProfileCallback(person) {
+  var filenames = person.files;
+  // var options = {
+  //     'displayName': person.displayName,
+  //     'imageUrl': person.image.url + '0',
+  // };
+  //showProfileNotification(options);
+}
+
+/**
+* Get details about the users Gmail inbox.
+*
+* https://developers.google.com/gmail/api/v1/reference/users/labels/get
+*
+* @param {string} token - Current users access_token.
+*/
+function updateLabelCount(token) {
+  get({
+      'url': 'https://www.googleapis.com/drive/v3/files?key={YOUR_API_KEY}',
+      //'url': 'https://www.googleapis.com/drive/v3/',
+      'callback': updateLabelCountCallback,
+      'token': token,
+  });
+}
+
+/**
+* Got users Gmail inbox details.
+*
+* https://developers.google.com/gmail/api/v1/reference/users/labels/get
+*
+* @param {object} label - Gmail users.labels resource.
+*/
+function updateLabelCountCallback(label) {
+  console.log("show api response");
+  console.log(label.files);
+  //setBadgeCount(label.threadsUnread);
+}
+
+/**
+* Make an authenticated HTTP GET request.
+*
+* @param {object} options
+*   @value {string} url - URL to make the request to. Must be whitelisted in manifest.json
+*   @value {string} token - Google access_token to authenticate request with.
+*   @value {function} callback - Function to receive response.
+*/
+function get(options) {
+  var xhr = new XMLHttpRequest();
+
+
+
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          // JSON response assumed. Other APIs may have different responses.
+          options.callback(JSON.parse(xhr.responseText));
+					// green
+					chrome.browserAction.setIcon({path:"img/green.png"});
+      } else {
+					// red
+					if (xhr.status == 401){
+						chrome.browserAction.setIcon({path:"img/red.png"});
+						console.log("there is a error");
+					}
+					else{
+						chrome.browserAction.setIcon({path:"img/green.png"});
+					}
+          console.log('get', xhr.readyState, xhr.status, xhr.responseText);
+      }
+  };
+  xhr.open("GET", options.url, true);
+  // Set standard Google APIs authentication header.
+  xhr.setRequestHeader('Authorization', 'Bearer ' + options.token);
+  xhr.send();
+}
+
+/**
+* Set browserAction status.
+*
+* @param {object} options
+*   @value {string} text - Up to four letters to be visible on browserAction.
+*   @value {string} color - Text background color. E.g. #FF0000
+*   @value {string} title - The hover tooltip.
+*/
+function setBadge(options) {
+  chrome.browserAction.setBadgeText({ 'text': options.text });
+  chrome.browserAction.setBadgeBackgroundColor({ 'color': options.color });
+  chrome.browserAction.setTitle({ 'title': options.title });
+}
+
+/**
+* Set the browserAction status for unauthenticated user.
+*/
+function setBadgeNoAuth() {
+  setBadge({
+      'text': '?',
+      'color': '#9E9E9E',
+      'title': 'Click to authorize Gmail',
+  });
+}
+
+/**
+* Set the browserAction status for Gmail label count.
+*
+* @param {int} count - The count of unread emails in label.
+*/
+function setBadgeCount(count) {
+  var color = '#9E9E9E';
+  var title = 'No unread mail';
+  if (count > 0) {
+      color = '#F44336';
+      title = count + ' unread mail';
+  }
+  setBadge({
+      'text': count + '', // Cast count int to string.
+      'color': color,
+      'title': title,
+  });
+}
+
+/**
+* User clicked on browserAction button. Check if user is authenticated.
+*
+* @param {object} tab - Chrome tab resource.
+*/
+function browserActionClicked(tab) {
+  getAuthToken({
+      'interactive': false,
+      'callback': getBrowserActionAuthTokenCallback,
+  });
+}
+
+/**
+* If user is authenticated open Gmail in new tab or start auth flow.
+*
+* @param {string} token - Current users access_token.
+*/
+function getBrowserActionAuthTokenCallback(token) {
+  if (chrome.runtime.lastError) {
+      getAuthTokenInteractive();
+  } else {
+      chrome.tabs.create({ 'url': 'https://drive.google.com' });
+  }
+
+}
+
+/**
+* Chrome alarm has triggered.
+*
+* @param {object} alarm - Chrome alarm resource.
+*/
+function onAlarm(alarm) {
+  // Check Gmail for current unread count.
+  if (alarm.name === 'update-count') {
+      getAuthTokenSilent();
+  }
+}
+
+/**
+* Wire up Chrome event listeners.
+*/
+//chrome.notifications.onClicked.addListener(notificationClicked);
+//chrome.browserAction.onClicked.addListener(browserActionClicked);
+//chrome.alarms.onAlarm.addListener(onAlarm);
+
+/**
+* Perform initial auth checks and set alarm for periodic updates.
+*/
+//setBadgeNoAuth();
+getAuthTokenSilent();
+//chrome.alarms.create('update-count', { 'delayInMinutes': 15, 'periodInMinutes': 15 });
+
+var HttpClient = function() {
+	this.get = function(aUrl, aCallback) {
+			var anHttpRequest = new XMLHttpRequest();
+			anHttpRequest.onreadystatechange = function() {
+					if (anHttpRequest.readyState === 4)
+							aCallback(anHttpRequest.status);
+			}
+			anHttpRequest.open( "GET", aUrl, true );
+			anHttpRequest.send();
+	}
+}
 
 
 window.onload = function(){
-	
-    //handleClientLoad();
-
-	/*
+	// window.setInterval(() => {
+	// 	getAuthTokenSilent();
+	// 	var client = new HttpClient();
+	// 	var flag = false;
+	// 	client.get('http://www.google.com', function(response) {
+  //     if (response === 200) {
+	// 			//getAuthTokenInteractive();
+  //       console.log("internet success");
+  //       flag = true;
+  //     } else {
+	// 			chrome.browserAction.setIcon({path:"img/yellow.png"});
+  //       flag = false;
+  //     }
+  //   });
+	// }, 1000);
 	chrome.storage.local.get(['log'], function(result) {
 		console.log(result['log']);
 		var i;
@@ -33,90 +375,4 @@ window.onload = function(){
 			document.getElementById('log').innerHTML += "</p>"
 		}
 	});
-	*/
 };
-
-function listFiles() {
-	gapi.client.drive.files.list({
-	  'pageSize': 10,
-	  'fields': "nextPageToken, files(id, name)"
-	}).then(function(response) {
-	  appendPre('Files:');
-	  var files = response.result.files;
-	  if (files && files.length > 0) {
-		for (var i = 0; i < files.length; i++) {
-		  var file = files[i];
-		  appendPre(file.name + ' (' + file.id + ')');
-		}
-	  } else {
-		appendPre('No files found.');
-	  }
-	});
-  }
-
-document.onreadystatechange = function () {
-	if (document.readyState === "complete") {
-		handleClientLoad();
-	}
-  }
-
-function appendPre(message) {
-	var pre = document.getElementById('content');
-	var textContent = document.createTextNode(message + '\n');
-	pre.appendChild(textContent);
-}
-
-function handleClientLoad() {
-	console.log("here is handle client load");
-	gapi.load('client:auth2', initClient);
-}
-
-var googleAuth = new OAuth2('google', {
-	client_id: CLIENT_ID,
-	client_secret: CLIENT_SECRET,
-	api_scope: SCOPES
-  });
-  
-  googleAuth.authorize(function() {
-	initClient();
-  });
-
-function initClient() {
-	console.log("very beginning of client init");
-	gapi.client.init({
-	  apiKey: API_KEY,
-	  clientId: CLIENT_ID,
-	  discoveryDocs: DISCOVERY_DOCS,
-	  scope: SCOPES
-	}).then(function () {
-	  console.log("here is client init");
-	  // Listen for sign-in state changes.
-	  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-	  // Handle the initial sign-in state.
-	  updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-	  authorizeButton.onclick = handleAuthClick;
-	  signoutButton.onclick = handleSignoutClick;
-	}).catch((error)=>{
-		console.log("error in init", error);
-	});
-  }
-
-  function updateSigninStatus(isSignedIn) {
-	if (isSignedIn) {
-	  authorizeButton.style.display = 'none';
-	  signoutButton.style.display = 'block';
-	  listFiles();
-	} else {
-	  authorizeButton.style.display = 'block';
-	  signoutButton.style.display = 'none';
-	}
-  }
-
-  function handleAuthClick(event) {
-	gapi.auth2.getAuthInstance().signIn();
-  }
-
-  function handleSignoutClick(event) {
-	gapi.auth2.getAuthInstance().signOut();
-  }
--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,323 @@
+/**
+ * Create a basic Desktop notification.
+ *
+ * @param {object} options
+ *   @value {string} iconUrl - Image URL to display in notification.
+ *   @value {string} title - Notification header.
+ *   @value {string} message - Notification message.
+ */
+
+function createBasicNotification(options) {
+  var notificationOptions = {
+      'type': 'basic',
+      'iconUrl': options.iconUrl, // Relative to Chrome dir or remote URL must be whitelisted in manifest.
+      'title': options.title,
+      'message': options.message,
+      'isClickable': true,
+  };
+  chrome.notifications.create(options.id, notificationOptions, function (notificationId) { });
+}
+
+/**
+* Show a notification that prompts the user to authenticate their Google account.
+*/
+function showAuthNotification() {
+  var options = {
+      'id': 'start-auth',
+      'iconUrl': 'img/developers-logo.png',
+      'title': 'GDE Sample: Chrome extension Google APIs',
+      'message': 'Click here to authorize access to Gmail',
+  };
+  createBasicNotification(options);
+}
+
+/**
+* Show a notification that authentication was completed successfully.
+*
+* @param {object} profile
+*   @value {string} imageUrl - Google+ profile image URL.
+*   @value {string} displayName - Google+ profile full name.
+*/
+function showProfileNotification(profile) {
+  var options = {
+      'id': 'show-profile',
+      'iconUrl': profile.imageUrl,
+      'title': 'Welcome ' + profile.displayName,
+      'message': 'Gmail checker is now active',
+  };
+  createBasicNotification(options);
+}
+
+/**
+* Triggered anytime user clicks on a desktop notification.
+*/
+function notificationClicked(notificationId) {
+  // User clicked on notification to start auth flow.
+  if (notificationId === 'start-auth') {
+      getAuthTokenInteractive();
+  }
+  clearNotification(notificationId);
+}
+
+/**
+* Clear a desktop notification.
+*
+* @param {string} notificationId - Id of notification to clear.
+*/
+function clearNotification(notificationId) {
+  chrome.notifications.clear(notificationId, function (wasCleared) { });
+}
+
+/**
+* Get users access_token.
+*
+* @param {object} options
+*   @value {boolean} interactive - If user is not authorized ext, should auth UI be displayed.
+*   @value {function} callback - Async function to receive getAuthToken result.
+*/
+function getAuthToken(options) {
+  chrome.identity.getAuthToken({ 'interactive': options.interactive }, options.callback);
+}
+
+/**
+* Get users access_token in background with now UI prompts.
+*/
+function getAuthTokenSilent() {
+  getAuthToken({
+      'interactive': false,
+      'callback': getAuthTokenSilentCallback,
+  });
+}
+
+/**
+* Get users access_token or show authorize UI if access has not been granted.
+*/
+function getAuthTokenInteractive() {
+  getAuthToken({
+      'interactive': true,
+      'callback': getAuthTokenInteractiveCallback,
+  });
+}
+
+/**
+* If user is authorized, start getting Gmail count.
+*
+* @param {string} token - Users access_token.
+*/
+function getAuthTokenSilentCallback(token) {
+  // Catch chrome error if user is not authorized.
+  if (chrome.runtime.lastError) {
+      showAuthNotification();
+  } else {
+      updateLabelCount(token);
+  }
+}
+
+/**
+* User finished authorizing, start getting Gmail count.
+*
+* @param {string} token - Current users access_token.
+*/
+function getAuthTokenInteractiveCallback(token) {
+  // Catch chrome error if user is not authorized.
+  if (chrome.runtime.lastError) {
+      showAuthNotification();
+  } else {
+      updateLabelCount(token);
+      //getProfile(token);
+  }
+}
+
+/**
+* Get the current users Google+ profile to welcome them.
+*
+* https://developers.google.com/+/api/latest/people/get
+*
+* @param {string} token - Current users access_token.
+*/
+function getProfile(token) {
+  get({
+      // 'url':'https://www.googleapis.com/',
+      'url': 'https://www.googleapis.com/drive/v3/files?key={YOUR_API_KEY}',
+      'callback': getProfileCallback,
+      'token': token,
+  });
+}
+
+/**
+* Got users Google+ profile, show welcome desktop notification.
+*
+* https://developers.google.com/+/api/latest/people/get
+*
+* @param {object} person - Google+ person resource.
+*/
+function getProfileCallback(person) {
+  var filenames = person.files;
+  // var options = {
+  //     'displayName': person.displayName,
+  //     'imageUrl': person.image.url + '0',
+  // };
+  //showProfileNotification(options);
+}
+
+/**
+* Get details about the users Gmail inbox.
+*
+* https://developers.google.com/gmail/api/v1/reference/users/labels/get
+*
+* @param {string} token - Current users access_token.
+*/
+function updateLabelCount(token) {
+  get({
+      'url': 'https://www.googleapis.com/drive/v3/files?key={YOUR_API_KEY}',
+      //'url': 'https://www.googleapis.com/drive/v3/',
+      'callback': updateLabelCountCallback,
+      'token': token,
+  });
+}
+
+/**
+* Got users Gmail inbox details.
+*
+* https://developers.google.com/gmail/api/v1/reference/users/labels/get
+*
+* @param {object} label - Gmail users.labels resource.
+*/
+function updateLabelCountCallback(label) {
+  console.log("show api response");
+  console.log(label.files);
+  //setBadgeCount(label.threadsUnread);
+}
+
+/**
+* Make an authenticated HTTP GET request.
+*
+* @param {object} options
+*   @value {string} url - URL to make the request to. Must be whitelisted in manifest.json
+*   @value {string} token - Google access_token to authenticate request with.
+*   @value {function} callback - Function to receive response.
+*/
+function get(options) {
+  var xhr = new XMLHttpRequest();
+
+
+
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          // JSON response assumed. Other APIs may have different responses.
+          options.callback(JSON.parse(xhr.responseText));
+          // green
+      } else {
+          // red
+          console.log('get', xhr.readyState, xhr.status, xhr.responseText);
+          console.log("there is a error");
+      }
+  };
+  xhr.open("GET", options.url, true);
+  // Set standard Google APIs authentication header.
+  xhr.setRequestHeader('Authorization', 'Bearer ' + options.token);
+  xhr.send();
+}
+
+/**
+* Set browserAction status.
+*
+* @param {object} options
+*   @value {string} text - Up to four letters to be visible on browserAction.
+*   @value {string} color - Text background color. E.g. #FF0000
+*   @value {string} title - The hover tooltip.
+*/
+function setBadge(options) {
+  chrome.browserAction.setBadgeText({ 'text': options.text });
+  chrome.browserAction.setBadgeBackgroundColor({ 'color': options.color });
+  chrome.browserAction.setTitle({ 'title': options.title });
+}
+
+/**
+* Set the browserAction status for unauthenticated user.
+*/
+function setBadgeNoAuth() {
+  setBadge({
+      'text': '?',
+      'color': '#9E9E9E',
+      'title': 'Click to authorize Gmail',
+  });
+}
+
+/**
+* Set the browserAction status for Gmail label count.
+*
+* @param {int} count - The count of unread emails in label.
+*/
+function setBadgeCount(count) {
+  var color = '#9E9E9E';
+  var title = 'No unread mail';
+  if (count > 0) {
+      color = '#F44336';
+      title = count + ' unread mail';
+  }
+  setBadge({
+      'text': count + '', // Cast count int to string.
+      'color': color,
+      'title': title,
+  });
+}
+
+/**
+* User clicked on browserAction button. Check if user is authenticated.
+*
+* @param {object} tab - Chrome tab resource.
+*/
+function browserActionClicked(tab) {
+  getAuthToken({
+      'interactive': false,
+      'callback': getBrowserActionAuthTokenCallback,
+  });
+}
+
+/**
+* If user is authenticated open Gmail in new tab or start auth flow.
+*
+* @param {string} token - Current users access_token.
+*/
+function getBrowserActionAuthTokenCallback(token) {
+  if (chrome.runtime.lastError) {
+      getAuthTokenInteractive();
+  } else {
+      chrome.tabs.create({ 'url': 'https://drive.google.com' });
+  }
+
+}
+
+/**
+* Chrome alarm has triggered.
+*
+* @param {object} alarm - Chrome alarm resource.
+*/
+function onAlarm(alarm) {
+  // Check Gmail for current unread count.
+  if (alarm.name === 'update-count') {
+      getAuthTokenSilent();
+  }
+}
+
+/**
+* Wire up Chrome event listeners.
+*/
+//chrome.notifications.onClicked.addListener(notificationClicked);
+chrome.browserAction.onClicked.addListener(browserActionClicked);
+chrome.alarms.onAlarm.addListener(onAlarm);
+
+/**
+* Perform initial auth checks and set alarm for periodic updates.
+*/
+setBadgeNoAuth();
+getAuthTokenInteractive();
+chrome.alarms.create('update-count', { 'delayInMinutes': 15, 'periodInMinutes': 15 });
+
+
+
+
 var condition;
 var trueCondition;
 var flag = true;
@@ -55,7 +375,7 @@ function addToLog(message) {
 var client = new HttpClient();
 var info = []
 
-chrome.storage.local.get(['log'], function() {
+chrome.storage.local.get(['log'], function(result) {
   if (typeof result.log === "undefined") {
     chrome.storage.local.set({'log': info});
   }
@@ -64,6 +384,8 @@ chrome.storage.local.get(['log'], function() {
 
     client.get('http://www.google.com', function(response) {
       if (response === 200) {
+        //getAuthTokenInteractive();
+        console.log("internet success");
         flag = true;
       } else {
         flag = false;
@@ -99,6 +421,6 @@ chrome.storage.local.get(['log'], function() {
         prev_condition = trueCondition;
       }
     }
-    chrome.browserAction.setIcon({path: trueCondition + ".png"});
+    chrome.browserAction.setIcon({path: "img/"+trueCondition + ".png"});
   }, 1000);
 });
