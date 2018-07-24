0df99d1fc1dcac095e97daa9417226500577e1b5
root <root@HOME.localdomain>
2018-07-24 15:26:13 -0400

    sign in

--- oauth.js
+++ oauth.js
@@ -5,4 +5,16 @@ window.onload = function () {
             console.log(token);
         });
     });
+
+    document.getElementById('signout_button').addEventListener('click', function () {
+        console.log("click sign out button");
+        var url = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://www.example.com";
+        chrome.tabs.create({ url: url });  
+        // chrome.identity.getAuthToken({ interactive: true }, function (token) {
+        //     chrome.identity.removeCachedAuthToken({ token: token, signOut: true },
+        //         function () {
+        //             console.log("Cached token has been removed")
+        //         });
+        // });     
+    });
 };
