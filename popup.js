aa18d9527b10b18dbce5870a60fd22b33fb93da0
root <root@HOME.localdomain>
2018-07-31 14:41:29 -0400

    latest verison

--- popup.js
+++ popup.js
@@ -1,9 +1,50 @@
-// pop up a login panel for not logged in user to log in
-// LOGIN entrance
+function convertToCSV(args) {
+	console.log("test", args);
+
+	var result, lineDelimiter, data, i;
+
+	data = args.data || null;
+	if (data == null || !data.length) {
+		return null;
+	}
+
+	lineDelimiter = '\n';
+	result = '';
+	console.log("length ", data.length);
+
+	for (i = 0; i < data.length; i++) {
+		result += data[i];
+		result += lineDelimiter;
+	}
+	console.log(result);
+	return result;
+}
+
+function downloadCSV(args) {
+	var data, filename, link;
+
+	var csv = convertToCSV({ data: args });
+	data = args.data;
+	if (csv == null) return;
+
+	filename = 'export.csv';
+
+	if (!csv.match(/^data:text\/csv/i)) {
+		csv = 'data:text/csv;charset=utf-8,' + csv;
+	}
+	data = encodeURI(csv);
+
+	link = document.createElement('a');
+	link.setAttribute('href', data);
+	link.setAttribute('download', filename);
+	link.click();
+}
+
 window.onload = function () {
 	document.getElementById('drive_button').addEventListener('click', function () {
 		window.open("https://drive.google.com/drive/my-drive");
 	});
+	
 	document.getElementById('authorize_button').addEventListener('click', function () {
 		console.log("click sign in button");
 		chrome.identity.getAuthToken({ interactive: true }, function (token) {
@@ -11,6 +52,13 @@ window.onload = function () {
 		});
 	});
 
+	document.getElementById('POPUP').addEventListener('click', function () {
+		var popup = document.getElementById("myPopup");
+		popup.classList.toggle("show");
+	});
+
+
+	/*
 	document.getElementById('signout_button').addEventListener('click', function () {
 		console.log("click sign out button");
 		var url = "https://www.google.com/accounts/Logout";
@@ -22,15 +70,25 @@ window.onload = function () {
 		//         });
 		// });     
 	});
+	*/
+
 	chrome.storage.local.get(['log'], function (result) {
 		console.log(result['log']);
 		var i;
-		for (i = 0; i < result['log'].length; i++) {
+		var length = Math.min(result['log'].length, 10);
+
+		for (i = 0; i < length; i++) {
 			document.getElementById('log').innerHTML += "<p>"
 			document.getElementById('log').innerHTML += result['log'][i];
 			document.getElementById('log').innerHTML += "</p>"
 		}
+
+		document.getElementById('export_button').addEventListener('click', function () {
+			console.log("test", result['log']);
+			downloadCSV(result['log']);
+		})
 	});
 };
+
 
 
--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,5 @@
 var signed_in = false;
+var signed_in_display = false;
 var GFLAG = true;
 
 /**
@@ -18,8 +19,8 @@ function getAuthToken(options) {
 */
 function getAuthTokenInteractive() {
   getAuthToken({
-      'interactive': true,
-      'callback': getAuthTokenInteractiveCallback,
+    'interactive': true,
+    'callback': getAuthTokenInteractiveCallback,
   });
 }
 
@@ -33,9 +34,9 @@ function getAuthTokenInteractiveCallback(token) {
   if (!token) signed_in = false;
   else signed_in = true;
   if (chrome.runtime.lastError) {
-      console.log("not logged in");
+    console.log("not logged in");
   } else {
-      google_drive_api(token);
+    google_drive_api(token);
   }
 }
 
@@ -49,10 +50,10 @@ function getAuthTokenInteractiveCallback(token) {
 */
 function google_drive_api(token) {
   get({
-      'url': 'https://www.googleapis.com/drive/v3/files?key={YOUR_API_KEY}',
-      //'url': 'https://www.googleapis.com/drive/v3/',
-      'callback': google_drive_api_callback,
-      'token': token,
+    'url': 'https://www.googleapis.com/drive/v3/files?key={YOUR_API_KEY}',
+    //'url': 'https://www.googleapis.com/drive/v3/',
+    'callback': google_drive_api_callback,
+    'token': token,
   });
 }
 
@@ -65,7 +66,6 @@ function google_drive_api(token) {
 function google_drive_api_callback(label) {
   console.log("show api response");
   console.log(label.files);
-  //setBadgeCount(label.threadsUnread);
 }
 
 /**
@@ -79,18 +79,18 @@ function google_drive_api_callback(label) {
 function get(options) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-          // JSON response assumed. Other APIs may have different responses.
-          options.callback(JSON.parse(xhr.responseText));
-          // green
-          GFLAG = true;
-      } else {
-          if (xhr.status!=200 && xhr.status!=0){
-              GFLAG = false;
-              console.log('get', xhr.readyState, xhr.status, xhr.responseText);
-              console.log("there is a error");
-          }
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      // JSON response assumed. Other APIs may have different responses.
+      options.callback(JSON.parse(xhr.responseText));
+      // green
+      GFLAG = true;
+    } else {
+      if (xhr.status != 200 && xhr.status != 0) {
+        GFLAG = false;
+        console.log('get', xhr.readyState, xhr.status, xhr.responseText);
+        console.log("there is a error");
       }
+    }
   };
   xhr.open("GET", options.url, true);
   // Set standard Google APIs authentication header.
@@ -102,16 +102,34 @@ function get(options) {
 // pop up a login panel for not logged in user to log in
 // LOGIN entrance
 getAuthTokenInteractive();
-chrome.identity.getAuthToken({interactive: false}, function (token) {
+chrome.identity.getAuthToken({ interactive: false }, function (token) {
   if (!token) {
-      if (chrome.runtime.lastError.message.match(/not signed in/)) {
-          signed_in = false;
-      } else {
-          signed_in = true;
-      }
+    if (chrome.runtime.lastError.message.match(/not signed in/)) {
+      signed_in = false;
+    } else {
+      signed_in = true;
+    }
   }
 });
 console.log("sign in ", signed_in);
+
+
+chrome.runtime.onInstalled.addListener(function (callback) {
+  if (callback.reason == "install") {
+    //call a function to handle a first install
+    var message = "Extension installed at:&nbsp&nbsp";
+    message += timeStamp();
+    console.log(message);
+    addToLog(message);
+  } 
+  else if (callback.reason == "update") {
+    //call a function to handle an update
+    var message = "Extension updated at:&nbsp&nbsp";
+    message += timeStamp();
+    console.log(message);
+    addToLog(message);
+  }
+});
 
 
 
@@ -120,34 +138,34 @@ var trueCondition;
 var flag = true;
 var prev_condition = "green";
 
-var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() {
-            if (anHttpRequest.readyState === 4)
-                aCallback(anHttpRequest.status);
-        }
-        anHttpRequest.open( "GET", aUrl, true );
-        anHttpRequest.send();
+var HttpClient = function () {
+  this.get = function (aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function () {
+      if (anHttpRequest.readyState === 4)
+        aCallback(anHttpRequest.status);
     }
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send();
+  }
 }
 
 function timeStamp() {
   // Create a date object with the current time
   var now = new Date();
   // Create an array with the current month, day and time
-  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+  var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
   // Create an array with the current hour, minute and second
-  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+  var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
   // Determine AM or PM suffix based on the hour
-  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+  var suffix = (time[0] < 12) ? "AM" : "PM";
   // Convert hour from military time
-  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+  time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
   // If hour is 0, set it to 12
   time[0] = time[0] || 12;
   // If seconds and minutes are less than 10, add a zero
-  for ( var i = 1; i < 3; i++ ) {
-    if ( time[i] < 10 ) {
+  for (var i = 1; i < 3; i++) {
+    if (time[i] < 10) {
       time[i] = "0" + time[i];
     }
   }
@@ -156,33 +174,33 @@ function timeStamp() {
 }
 
 function addToLog(message) {
-  chrome.storage.local.get(['log'], function(result) {
+  chrome.storage.local.get(['log'], function (result) {
     var arr = result['log'];
     if (arr.length === 10) {
       arr.shift();
       arr.push(message);
     }
-    else if (arr.length < 10){
+    else if (arr.length < 10) {
       arr.push(message);
     }
-    chrome.storage.local.set({'log': arr});
+    chrome.storage.local.set({ 'log': arr });
   });
 }
 
 var client = new HttpClient();
 var info = []
 
-chrome.storage.local.get(['log'], function(result) {
+chrome.storage.local.get(['log'], function (result) {
   if (typeof result.log === "undefined") {
-    chrome.storage.local.set({'log': info});
+    chrome.storage.local.set({ 'log': info });
   }
   window.setInterval(() => {
     condition = navigator.onLine ? "online" : "offline";
-    client.get('http://www.google.com', function(response) {
+    client.get('http://www.google.com', function (response) {
       if (response === 200) {
-        console.log("internet success");  
+        console.log("internet success");
         console.log("sign in ", signed_in);
-        if (signed_in ===true){
+        if (signed_in === true) {
           getAuthTokenInteractive();
         }
         flag = true;
@@ -190,10 +208,17 @@ chrome.storage.local.get(['log'], function(result) {
         flag = false;
       }
     });
+    if (signed_in == true && signed_in_display==false) {
+      var message = "User signed in at:&nbsp&nbsp";
+      message += timeStamp();
+      console.log(message);
+      addToLog(message);
+      signed_in_display = true;
+    }
     if (flag === false) {
       trueCondition = "yellow";
       if (trueCondition !== prev_condition) {
-        var message = "Internet offline at:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ";
+        var message = "Internet offline at:&nbsp&nbsp";
         message += timeStamp();
         console.log(message);
         addToLog(message);
@@ -203,7 +228,7 @@ chrome.storage.local.get(['log'], function(result) {
       if (GFLAG === true) {
         trueCondition = "green";
         if (trueCondition !== prev_condition) {
-          var message = "Google drive up at: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+          var message = "Google drive up at:&nbsp";
           message += timeStamp();
           console.log(message);
           addToLog(message);
@@ -212,7 +237,7 @@ chrome.storage.local.get(['log'], function(result) {
       } else {
         trueCondition = "red";
         if (trueCondition !== prev_condition) {
-          var message = "Google drive down at:&nbsp;&nbsp;&nbsp;";
+          var message = "Google drive down at:";
           message += timeStamp();
           console.log(message);
           addToLog(message);
@@ -220,6 +245,6 @@ chrome.storage.local.get(['log'], function(result) {
         prev_condition = trueCondition;
       }
     }
-    chrome.browserAction.setIcon({path: "img/"+trueCondition + ".png"});
+    chrome.browserAction.setIcon({ path: "img/" + trueCondition + ".png" });
   }, 1000);
 });
